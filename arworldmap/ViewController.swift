14cb78f7f0e9051e752b3993ca908efd8f804c2e
Mika <mika@MikadeMacBook-Pro.local>
2021-11-06 14:15:25 +0100

    initial commit

---
+++ arworldmap/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  arworldmap
+//
+//
+
+import UIKit
+import SceneKit
+import ARKit
+import AVFoundation
+import CoreLocation
+
+class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDelegate, ARSessionDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var boxNode = SCNNode()
+    var boxNode1 = SCNNode()
+    var boxNode2 = SCNNode()
+    var scene =  SCNScene()
+    var didFindLocation = false
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        self.locationManager.requestAlwaysAuthorization()
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.requestWhenInUseAuthorization();
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        else{
+            print("Location service disabled");
+        }
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        //create a transparent gray layer
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+        
+        boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(0,0,-0.5)
+        scene.rootNode.addChildNode(boxNode)
+        
+        boxNode1 = SCNNode(geometry: box)
+        boxNode1.position = SCNVector3(-0.5,0,0)
+        scene.rootNode.addChildNode(boxNode1)
+
+        boxNode2 = SCNNode(geometry: box)
+        boxNode2.position = SCNVector3(0,-0.5,0)
+        scene.rootNode.addChildNode(boxNode2)
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        sceneView.delegate = self
+    }
+    /* This method creates only Text Nodes.
+     */
+    func createTextNode(title: String, size: CGFloat, x: Float, y: Float){
+        let text = SCNText(string: title, extrusionDepth: 0)
+        text.firstMaterial?.diffuse.contents = UIColor.white
+        text.font = UIFont(name: "Avenir Next", size: size)
+        let textNode = SCNNode(geometry: text)
+        textNode.position.x = boxNode.position.x - x
+        textNode.position.y = boxNode.position.y - y
+        textNode.position.z = boxNode.position.z - 50
+        scene.rootNode.addChildNode(textNode)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = AROrientationTrackingConfiguration()
+        configuration.worldAlignment = .gravityAndHeading
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        sceneView.session.delegate = self
+    }
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location:CLLocationCoordinate2D = manager.location?.coordinate {
+            print("Your location is \(location.latitude) \(location.longitude)")
+            manager.stopUpdatingLocation()
+            manager.delegate = nil
+            // Just some test text
+            self.createTextNode(title: "lat:\(location.latitude)", size: 1.8, x: 0, y: 9)
+            self.createTextNode(title: "lon:\(location.longitude)", size: 1.8, x: 0, y: 6)
+            self.createTextNode(title: "north", size: 1.8, x: 0, y: 0)
+        }
+    }
+}
