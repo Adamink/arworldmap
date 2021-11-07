ec82cdbceaa95afa9618a679a2bf309512b89442
Fiona730 <fiona0730pku@gmail.com>
2021-11-07 14:47:06 +0100

    Add coord transform

--- arworldmap/ViewController.swift
+++ arworldmap/ViewController.swift
@@ -52,13 +52,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
         boxNode.position = SCNVector3(0,0,-0.5)
         scene.rootNode.addChildNode(boxNode)
         
-        boxNode1 = SCNNode(geometry: box)
-        boxNode1.position = SCNVector3(-0.5,0,0)
-        scene.rootNode.addChildNode(boxNode1)
-
-        boxNode2 = SCNNode(geometry: box)
-        boxNode2.position = SCNVector3(0,-0.5,0)
-        scene.rootNode.addChildNode(boxNode2)
+//        boxNode1 = SCNNode(geometry: box)
+//        boxNode1.position = SCNVector3(-0.5,0,0)
+//        scene.rootNode.addChildNode(boxNode1)
+//
+//        boxNode2 = SCNNode(geometry: box)
+//        boxNode2.position = SCNVector3(0,-0.5,0)
+//        scene.rootNode.addChildNode(boxNode2)
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -66,15 +66,81 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
     }
     /* This method creates only Text Nodes.
      */
-    func createTextNode(title: String, size: CGFloat, x: Float, y: Float){
+    func createTextNode(title: String, size: CGFloat, x: Float, y: Float, z: Float){
         let text = SCNText(string: title, extrusionDepth: 0)
         text.firstMaterial?.diffuse.contents = UIColor.white
         text.font = UIFont(name: "Avenir Next", size: size)
         let textNode = SCNNode(geometry: text)
         textNode.position.x = boxNode.position.x - x
         textNode.position.y = boxNode.position.y - y
-        textNode.position.z = boxNode.position.z - 50
+        textNode.position.z = boxNode.position.z - z
         scene.rootNode.addChildNode(textNode)
+    }
+    
+    func createBoxNode(pos: SCNVector3){
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+        let boxMaterial = SCNMaterial()
+        boxMaterial.diffuse.contents = UIImage(named:"art.scnassets/sun.jpg")
+        box.materials = [boxMaterial]
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = pos
+        scene.rootNode.addChildNode(boxNode)
+    }
+    
+    func coordinateTransform(selfLat: CLLocationDegrees, selfLon: CLLocationDegrees, countryLat: CLLocationDegrees, countryLon: CLLocationDegrees) -> SCNVector3 {
+        let selfPosWorld = LatLonToXYZ(lat: selfLat, lon: selfLon)
+        let countryPosWorld = LatLonToXYZ(lat: countryLon, lon: countryLon)
+        var countryPosLocal = countryPosWorld - selfPosWorld
+        var transMtx = makeRotationMatrixAlongZ(angle: -(90-selfLon))
+        transMtx = simd_mul(makeRotationMatrixAlongX(angle: selfLat), transMtx)
+        transMtx = simd_mul(makeRotationMatrixAlongY(angle: 180), transMtx)
+        countryPosLocal = simd_normalize(simd_mul(transMtx.inverse, countryPosLocal))
+        return SCNVector3(countryPosLocal.x, countryPosLocal.y, countryPosLocal.z)
+    }
+    
+    func LatLonToXYZ(lat: CLLocationDegrees, lon: CLLocationDegrees) -> simd_double3 {
+        let radius = 6371.0
+        let x = radius * cos(lat * Double.pi / 180) * cos(lon * Double.pi / 180)
+        let y = radius * cos(lat * Double.pi / 180) * sin(lon * Double.pi / 180)
+        let z = radius * sin(lat * Double.pi / 180)
+        return simd_double3(x, y, z)
+    }
+    
+    func XYZToLatLon(x: Double, y: Double, z: Double) -> simd_double2 {
+        let radius = sqrt(x*x + y*y + z*z)
+        let lat = asin((z / radius) * Double.pi / 180)
+        let long = atan((y / x) * Double.pi / 180)
+        return simd_double2(lat, long)
+    }
+    
+    func makeRotationMatrixAlongX(angle: Double) -> simd_double3x3 {
+        let rows = [
+            simd_double3(1,     0,         0),
+            simd_double3(0,     cos(angle), -sin(angle)),
+            simd_double3(0,     sin(angle), cos(angle))
+        ]
+        
+        return simd_double3x3(rows: rows)
+    }
+    
+    func makeRotationMatrixAlongY(angle: Double) -> simd_double3x3 {
+        let rows = [
+            simd_double3(cos(angle),    0,     sin(angle)),
+            simd_double3(0,             1,     0),
+            simd_double3(-sin(angle),   0,      cos(angle))
+        ]
+        
+        return simd_double3x3(rows: rows)
+    }
+    
+    func makeRotationMatrixAlongZ(angle: Double) -> simd_double3x3 {
+        let rows = [
+            simd_double3(cos(angle), -sin(angle), 0),
+            simd_double3(sin(angle), cos(angle), 0),
+            simd_double3(0,          0,          1)
+        ]
+        
+        return simd_double3x3(rows: rows)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -106,9 +172,15 @@ extension ViewController: CLLocationManagerDelegate{
             manager.stopUpdatingLocation()
             manager.delegate = nil
             // Just some test text
-            self.createTextNode(title: "lat:\(location.latitude)", size: 1.8, x: 0, y: 9)
-            self.createTextNode(title: "lon:\(location.longitude)", size: 1.8, x: 0, y: 6)
-            self.createTextNode(title: "north", size: 1.8, x: 0, y: 0)
+            self.createTextNode(title: "lat:\(location.latitude)", size: 1.8, x: 0, y: 9, z: 10)
+            self.createTextNode(title: "lon:\(location.longitude)", size: 1.8, x: 0, y: 6, z: 50)
+            self.createTextNode(title: "north", size: 1.8, x: 0, y: 0, z: 50)
+            
+            // hard code position
+            let pos = self.coordinateTransform(selfLat: location.latitude, selfLon: location.longitude, countryLat: 23.0, countryLon: 102.0)
+            print("Mexico location is \(pos.x) \(pos.y) \(pos.z)")
+            // add box
+            self.createBoxNode(pos: pos)
         }
     }
 }
