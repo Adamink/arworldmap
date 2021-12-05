681f9b73d9949a1f65d9d319fc4f66d472d3c46c
sht <haishi@student.ethz.ch>
2021-12-05 18:15:39 +0100

    change dropdown position but country not showing

--- arworldmap/InfoViewController.swift
+++ arworldmap/InfoViewController.swift
@@ -20,6 +20,9 @@ class InfoViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererD
     
     let locationManager = CLLocationManager()
     
+    let dropDown = DropDown()
+    var searchButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        view.backgroundColor = .blue
@@ -38,6 +41,35 @@ class InfoViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererD
             print("Location service disabled");
         }
         
+        let midX = self.view.bounds.midX
+        let midY = self.view.bounds.midY
+        let rect1 = CGRect(x: midX - 80, y: midY - 130, width: 160, height: 70)
+        searchButton = UIButton(frame: rect1)
+        searchButton.setTitle("Search Position", for: .normal)
+        searchButton.addTarget(self, action: #selector(search), for: .touchUpInside)
+        let image = UIImage(named: "./art.scnassets/52016_preview.png")
+        searchButton.setBackgroundImage(image, for: UIControl.State.normal)
+        self.view.addSubview(searchButton)
+        
+        dropDown.dataSource = ["China", "Switzerland", "America"]
+        
+        // hard code position
+        // opposite side of the globe
+        let leftLon = 73.554302
+        let rightLon = 134.775703
+        let topLat = 53.561780
+        let bottomLat = 18.155060
+        let location = self.locationManager.location?.coordinate
+        
+        DropDown.appearance().setupCornerRadius(10)
+        DropDown.appearance().textFont = UIFont.systemFont(ofSize: 20)
+        dropDown.selectionAction = { [weak self] (index: Int, item: String) in
+            print("Selected item: \(item) at index: \(index)")
+            let mapPos = coordinateTransform(selfLat: location!.latitude, selfLon: location!.longitude, countryLat: (topLat+bottomLat)/2, countryLon: (leftLon+rightLon)/2)
+            self?.createMapNode(width: CGFloat(rightLon-leftLon)/90,
+                               height: CGFloat(topLat-bottomLat)/90, pos: SCNVector3(mapPos.x, mapPos.y, mapPos.z))
+        }
+        
         // Set the view's delegate
         searchView?.delegate = self
         
@@ -48,7 +80,38 @@ class InfoViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererD
         searchView?.scene = scene
     }
     
+    @objc func search(sender: UIButton!) {
+        dropDown.show()
+    }
     
+    func createMapNode(width : CGFloat, height: CGFloat,pos: SCNVector3){
+        let plane = SCNPlane(width: width, height: height)
+        let planeMaterial = SCNMaterial()
+        planeMaterial.diffuse.contents = UIImage(named:"art.scnassets/chinaHigh.png")
+        
+        plane.materials = [planeMaterial]
+        let mapNode = SCNNode(geometry: plane)
+        mapNode.position = pos
+        
+        // calculate eulerAngle
+        let planeDir = -simd_normalize(simd_double3((Double)(pos.x), (Double)(pos.y), (Double)(pos.z)) - 0)
+        let alpha = atan(planeDir.y / sqrt(planeDir.x*planeDir.x + planeDir.z*planeDir.z)) * 180 / Double.pi
+        let beta = atan(planeDir.x / planeDir.z) * 180 / Double.pi
+        var betaReformat = beta
+        if (planeDir.z < 0)
+        {
+            betaReformat = beta + 180
+        }
+        let xAngle = -alpha * Double.pi / 180
+        let yAngle = betaReformat * Double.pi / 180
+        mapNode.eulerAngles = SCNVector3(xAngle, yAngle, 0)
+        
+        self.scene.rootNode.addChildNode(mapNode)  // not shown
+        print("new node")
+        self.scene.rootNode.enumerateChildNodes { (node, stop) in
+            print(node)
+        }
+    }
 }
 
 
--- arworldmap/ViewController.swift
+++ arworldmap/ViewController.swift
@@ -8,7 +8,6 @@ import SceneKit
 import ARKit
 import AVFoundation
 import CoreLocation
-import DropDown
 import SideMenu
 
 class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDelegate, ARSessionDelegate, MenuControllerDelegate {
@@ -22,9 +21,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
     var didFindLocation = false
     
     let locationManager = CLLocationManager()
-    
-    let dropDown = DropDown()
-    var searchButton = UIButton()
     
     // to do: change content
     private var sideMenu: SideMenuNavigationController?
@@ -62,8 +58,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
         boxNode.position = SCNVector3(0,0,-0.5)
         boxNode.eulerAngles = SCNVector3(0,60,0)
 //        scene.rootNode.addChildNode(boxNode)
-        let midX = self.view.bounds.midX
-        let midY = self.view.bounds.midY
         
         let menu = MenuController(with: ["Home", "Info", "Settings"])
         menu.delegate = self
@@ -74,50 +68,18 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
         
         addChildControllers()
         
-        let rect1 = CGRect(x: midX - 80, y: midY - 130, width: 160, height: 70)
-        searchButton = UIButton(frame: rect1)
-        searchButton.setTitle("Search Position", for: .normal)
-        searchButton.addTarget(self, action: #selector(search), for: .touchUpInside)
-        let image = UIImage(named: "./art.scnassets/52016_preview.png")
-        searchButton.setBackgroundImage(image, for: UIControl.State.normal)
-        self.view.addSubview(searchButton)
-        
-        dropDown.dataSource = ["China", "Switzerland", "America"]
-        
-        // hard code position
-        // opposite side of the globe
-        let leftLon = 73.554302
-        let rightLon = 134.775703
-        let topLat = 53.561780
-        let bottomLat = 18.155060
-        let location = self.locationManager.location?.coordinate
-        
-        DropDown.appearance().setupCornerRadius(10)
-        DropDown.appearance().textFont = UIFont.systemFont(ofSize: 20)
-        dropDown.selectionAction = { [weak self] (index: Int, item: String) in
-            print("Selected item: \(item) at index: \(index)")
-            let mapPos = coordinateTransform(selfLat: location!.latitude, selfLon: location!.longitude, countryLat: (topLat+bottomLat)/2, countryLon: (leftLon+rightLon)/2)
-            self!.createMapNode(width: CGFloat(rightLon-leftLon)/90,
-                               height: CGFloat(topLat-bottomLat)/90, pos: SCNVector3(mapPos.x, mapPos.y, mapPos.z))
-        }
-        
         // Set the scene to the view
         sceneView.scene = scene
         sceneView.delegate = self
-    }
-    /* This method creates only Text Nodes.
-     */
-    @objc func search(sender: UIButton!) {
-        dropDown.show()
     }
     
     func didSelectMenuItem(named: String) {
         sideMenu?.dismiss(animated: true, completion: {[weak self] in
             
             self?.title = named
-            self?.scene.rootNode.enumerateChildNodes { (node, stop) in
-                    node.removeFromParentNode()
-            }
+//            self?.scene.rootNode.enumerateChildNodes { (node, stop) in
+//                    node.removeFromParentNode()
+//            }
             
             if named == "Home" {
                 self?.settingsController.view.isHidden = true
@@ -175,40 +137,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
         let boxNode = SCNNode(geometry: box)
         boxNode.position = pos
         scene.rootNode.addChildNode(boxNode)
-    }
-    
-    func createMapNode(width : CGFloat, height: CGFloat,pos: SCNVector3){
-        let plane = SCNPlane(width: width, height: height)
-//        let sphere = SCNSphere(radius: 0.5)
-        let planeMaterial = SCNMaterial()
-        planeMaterial.diffuse.contents = UIImage(named:"art.scnassets/chinaHigh.png")
-//        planeMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(0.1, 0.1, 0.1)
-        // planeMaterial.isDoubleSided = true
-        
-        plane.materials = [planeMaterial]
-        let mapNode = SCNNode(geometry: plane)
-        mapNode.position = pos
-//        mapNode.position = SCNVector3(1, -1, 0)
-        // some random angle
-        // mapNode.eulerAngles = SCNVector3(0, 90 / 180 * Double.pi, 0)
-        
-        // calculate eulerAngle
-        let planeDir = -simd_normalize(simd_double3((Double)(pos.x), (Double)(pos.y), (Double)(pos.z)) - 0)
-        let alpha = atan(planeDir.y / sqrt(planeDir.x*planeDir.x + planeDir.z*planeDir.z)) * 180 / Double.pi
-        // let beta = atan(planeDir.z / planeDir.x) * 180 / Double.pi
-        let beta = atan(planeDir.x / planeDir.z) * 180 / Double.pi
-        var betaReformat = beta
-        // if (planeDir.x < 0)
-        if (planeDir.z < 0)
-        {
-            betaReformat = beta + 180
-        }
-        let xAngle = -alpha * Double.pi / 180
-        // let yAngle = (90-betaReformat) * Double.pi / 180
-        let yAngle = betaReformat * Double.pi / 180
-        mapNode.eulerAngles = SCNVector3(xAngle, yAngle, 0)
-        
-        scene.rootNode.addChildNode(mapNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
